c757850739abcb3f652cec4c31edc00cc58d61a3
Jose Alejandro Mestre <jose.mestre@stu.jefferson.kyschools.us>
2018-03-19 08:08:21 -0400

    DictionaryProgram

---
+++ DictionaryProgram.py
@@ -0,0 +1,26 @@
+soccer_terms = {
+    
+    "Forward": "offensive position usually placed behind the striker; this player uses speed to make crisp accurate passes",
+    
+    "Right Back": "defensive position that covers the right side of the field",
+    
+    "Right Midfielder": "center position; this player tries to take the ball from the opponent before that player reaches the defenders and quickly make a counter attack",
+    
+    "Goal Keeper": "person whose role is to prevent the ball from entering the goal",
+    
+    "Stiker": "offensive position whose main role is to score goals",
+    
+    "Left Midfielder": "center position that plays on the left side of the field; this player uses playmaking skills to pass the ball to the forwards",
+    
+    "Defensive Midfielder": "center position; player that tries to take the ball from the opponent before that player reaches the defenders and quickly mount a counterattack",
+    
+    "Left Back": "defensive position that covers the left side of the field",
+    
+    "Stopper": "player that stays back, marks an opposing forward and prevents that player from being in a scoring position",
+    
+    "Sweeper": "player that stays back, anticipates defensive mistakes by teammates and makes up for them",
+    
+    }
+
+for key in soccer_terms:
+    print str(key) + "; " + str(soccer_terms[key])
